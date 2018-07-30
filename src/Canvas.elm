7958d68b062627cabf4c1fc7e033aac13cc1a782
Joakin <joaquin@chimeces.com>
2018-07-30 18:25:12 +0200

    Add documentation for Canvas methods

--- src/Canvas.elm
+++ src/Canvas.elm
@@ -154,7 +154,7 @@ type alias Command =
             , Canvas.fillStyle (Color.rgba 0 0 0 1)
             , List.range 0 100
                 |> List.map (renderItem time)
-                |> batch
+                |> Canvas.batch
             ]
 
 -}
@@ -725,43 +725,113 @@ textBaseline baseline =
 -- Methods
 
 
-{-| -}
+{-| Adds an arc to the path which is centered at (`x`, `y`) position with
+`radius` starting at `startAngle` and ending at `endAngle` going in the given
+direction by `anticlockwise` (`False` is clockwise). [MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arc)
+
+    [ beginPath
+    , arc x y radius startAngle endAngle anticlockwise
+    , stroke
+    ]
+
+-}
 arc : Float -> Float -> Float -> Float -> Float -> Bool -> Command
 arc x y radius startAngle endAngle anticlockwise =
     fn "arc" [ float x, float y, float radius, float 0, float (2 * pi), bool anticlockwise ]
 
 
-{-| -}
+{-| Adds an arc to the path with the given control points and radius.
+
+The arc drawn will be a part of a circle, never elliptical. Typical use could be
+making a rounded corner.
+
+One way to think about the arc drawn is to imagine two straight segments, from
+the starting point (latest point in current path) to the first control point,
+and then from the first control point to the second control point. These two
+segments form a sharp corner with the first control point being in the corner.
+Using `arcTo`, the corner will instead be an arc with the given radius.
+
+The arc is tangential to both segments, which can sometimes produce surprising
+results, e.g. if the radius given is larger than the distance between the
+starting point and the first control point.
+
+If the radius specified doesn't make the arc meet the starting point (latest
+point in the current path), the starting point is connected to the arc with
+a straight line segment.
+
+    arcTo x1 y1 x2 y2 radius
+
+[MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arcTo)
+
+-}
 arcTo : Float -> Float -> Float -> Float -> Float -> Command
 arcTo x1 y1 x2 y2 radius =
-    fn "arc" [ float x1, float y1, float x2, float y2, float radius ]
+    fn "arcTo" [ float x1, float y1, float x2, float y2, float radius ]
 
 
-{-| -}
+{-| Starts a new path by emptying the list of sub-paths. Call this method when
+you want to create a new path. [MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/beginPath)
+-}
 beginPath : Command
 beginPath =
     fn "beginPath" []
 
 
-{-| -}
+{-| Adds a cubic Bézier curve to the path. It requires three points. The first
+two points are control points and the third one is the end point. The starting
+point is the last point in the current path, which can be changed using `moveTo`
+before creating the Bézier curve. [MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/bezierCurveTo)
+
+    bezierCurveTo cp1x cp1y cp2x cp2y x y
+
+  - `cp1x`
+      - The x axis of the coordinate for the first control point.
+  - `cp1y`
+      - The y axis of the coordinate for the first control point.
+  - `cp2x`
+      - The x axis of the coordinate for the second control point.
+  - `cp2y`
+      - The y axis of the coordinate for the second control point.
+  - `x`
+      - The x axis of the coordinate for the end point.
+  - `y`
+      - The y axis of the coordinate for the end point.
+
+-}
 bezierCurveTo : Float -> Float -> Float -> Float -> Float -> Float -> Command
 bezierCurveTo cp1x cp1y cp2x cp2y x y =
     fn "bezierCurveTo" [ float cp1x, float cp1y, float cp2x, float cp2y, float x, float y ]
 
 
-{-| -}
+{-| Sets all pixels in the rectangle defined by starting point (`x`, `y`) and
+size (`width`, `height`) to transparent black, erasing any previously drawn
+content. [MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/clearRect)
+
+    clearRect x y width height
+
+A common problem with `clearRect` is that it may appear it does not work when
+not using paths properly. Don't forget to call `beginPath` before starting to
+draw the new frame after calling `clearRect`.
+
+-}
 clearRect : Float -> Float -> Float -> Float -> Command
 clearRect x y width height =
     fn "clearRect" [ float x, float y, float width, float height ]
 
 
-{-| -}
+{-| Type that represents the algorithm by which to determine if a point is
+inside a path or outside a path. If you are not sure, use `NonZero` as it is
+the default in the DOM API.
+
+  - `NonZero`: The [non-zero winding rule](http://en.wikipedia.org/wiki/Nonzero-rule).
+  - `EvenOdd`: The [even-odd winding rule](http://en.wikipedia.org/wiki/Even%E2%80%93odd_rule).
+
+-}
 type FillRule
     = NonZero
     | EvenOdd
 
 
-{-| -}
 fillRuleToString : FillRule -> String
 fillRuleToString fillRule =
     case fillRule of
@@ -772,27 +842,55 @@ fillRuleToString fillRule =
             "evenodd"
 
 
-{-| -}
+{-| Turns the path currently being built into the current clipping path.
+[MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/clip)
+
+Note: Be aware that `clip` only works with shapes added to the path; it doesn't
+work with shape primitives such as rectangles created with `fillRect`. In this
+case you'd have to use `rect` to draw a rectangular path shape to be clipped.
+
+-}
 clip : FillRule -> Command
 clip fillRule =
-    fn "clip"
-        [ string (fillRuleToString fillRule) ]
+    fn "clip" [ string (fillRuleToString fillRule) ]
 
 
-{-| -}
+{-| Causes the point of the pen to move back to the start of the current
+sub-path. It tries to add a straight line (but does not actually draw it) from
+the current point to the start. If the shape has already been closed or has
+only one point, this function does nothing.
+[MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/closePath)
+
+    [ beginPath
+    , moveTo 20 20
+    , lineTo 200 20
+    , lineTo 120 120
+    , closePath -- draws last line of the triangle
+    , stroke
+    ]
+
+-}
 closePath : Command
 closePath =
     fn "closePath" []
 
 
-{-| -}
+{-| Fills the current or given path with the current fill style using the
+non-zero or even-odd winding rule. See type `FillRule`.
+[MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fill)
+-}
 fill : FillRule -> Command
 fill fillRule =
-    fn "fill"
-        [ string (fillRuleToString fillRule) ]
+    fn "fill" [ string (fillRuleToString fillRule) ]
 
 
-{-| -}
+{-| This is a helper non-standard method to create a circle. Normally, you'd
+need to call `beginPath`, `arc` with the correct arguments, and `fill`. This is
+a convenience function to easily fill a circle that mirrors `fillRect`.
+
+    fillCircle x y radius
+
+-}
 fillCircle : Float -> Float -> Float -> Command
 fillCircle x y r =
     batch
@@ -802,13 +900,38 @@ fillCircle x y r =
         ]
 
 
-{-| -}
+{-| Draws a filled rectangle whose starting point is at the coordinates (`x`, `y`)
+with the specified `width` and `height` and whose style is determined by the
+fillStyle attribute. [MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillRect)
+
+    fillRect x y width height
+
+-}
 fillRect : Float -> Float -> Float -> Float -> Command
 fillRect x y w h =
     fn "fillRect" [ float x, float y, float w, float h ]
 
 
-{-| -}
+{-| Draws a text string at the specified coordinates, filling the string's
+characters with the current foreground color. An optional parameter allows
+specifying a maximum width for the rendered text, which the user agent will
+achieve by condensing the text or by using a lower font size.
+
+The text is rendered using the font and text layout configuration as defined by
+the `font`, `textAlign`, `textBaseline`, and `direction` properties.
+
+To draw the outlines of the characters in a string, call the context's
+`strokeText` method.
+
+    fillText text x y maxWidth
+
+[MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillText)
+
+    [ font "48px serif"
+    , fillText "Hello world" 50 100 Nothing
+    ]
+
+-}
 fillText : String -> Float -> Float -> Maybe Float -> Command
 fillText text x y maxWidth =
     case maxWidth of
@@ -819,79 +942,216 @@ fillText text x y maxWidth =
             fn "fillText" [ string text, float x, float y, float maxWidth ]
 
 
-{-| -}
+{-| Connects the last point in the sub-path to the x, y coordinates with a
+straight line (but does not actually draw it). [MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/lineTo)
+
+    lineTo x y
+
+-}
 lineTo : Float -> Float -> Command
 lineTo x y =
     fn "lineTo" [ float x, float y ]
 
 
-{-| -}
+{-| Moves the starting point of a new sub-path to the (x, y) coordinates.
+[MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/moveTo)
+
+    moveTo x y
+
+-}
 moveTo : Float -> Float -> Command
 moveTo x y =
     fn "moveTo" [ float x, float y ]
 
 
-{-| -}
+{-| Adds a quadratic Bézier curve to the path. It requires two points. The
+first point is a control point and the second one is the end point. The
+starting point is the last point in the current path, which can be changed
+using `moveTo` before creating the quadratic Bézier curve. [MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/quadraticCurveTo)
+
+    quadraticCurveTo cpx cpy x y
+
+  - `cpx`
+      - The x axis of the coordinate for the control point.
+  - `cpy`
+      - The y axis of the coordinate for the control point.
+  - `x`
+      - The x axis of the coordinate for the end point.
+  - `y`
+      - The y axis of the coordinate for the end point.
+
+-}
 quadraticCurveTo : Float -> Float -> Float -> Float -> Command
 quadraticCurveTo cpx cpy x y =
     fn "quadraticCurveTo" [ float cpx, float cpy, float x, float y ]
 
 
-{-| -}
+{-| Creates a path for a rectangle at position (`x`, `y`) with a size that is
+determined by `width` and `height`. Those four points are connected by straight
+lines and the sub-path is marked as closed, so that you can `fill` or `stroke`
+this rectangle. [MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/rect)
+
+    rect x y width height
+
+-}
 rect : Float -> Float -> Float -> Float -> Command
 rect x y w h =
     fn "rect" [ float x, float y, float w, float h ]
 
 
-{-| -}
+{-| Restores the most recently saved canvas state by popping the top entry in
+the drawing state stack. If there is no saved state, this method does nothing.
+[MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/restore)
+-}
 restore : Command
 restore =
     fn "restore" []
 
 
-{-| -}
+{-| Adds a rotation to the transformation matrix. The `angle` argument
+represents a clockwise rotation angle and is expressed in radians.
+
+    rotate (degrees 90)
+
+The rotation center point is always the canvas origin. To change the center
+point, we will need to move the canvas by using the `translate` method.
+
+[MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/rotate)
+
+-}
 rotate : Float -> Command
 rotate angle =
     fn "rotate" [ float angle ]
 
 
-{-| -}
+{-| Saves the entire state of the canvas by pushing the current state onto
+a stack. [MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/save)
+
+The drawing state that gets saved onto a stack consists of:
+
+  - The current transformation matrix.
+  - The current clipping region.
+  - The current dash list.
+  - The current values of the following attributes: strokeStyle, fillStyle, globalAlpha, lineWidth, lineCap, lineJoin, miterLimit, lineDashOffset, shadowOffsetX, shadowOffsetY, shadowBlur, shadowColor, globalCompositeOperation, font, textAlign, textBaseline, direction, imageSmoothingEnabled.
+
+-}
 save : Command
 save =
     fn "save" []
 
 
-{-| -}
+{-| Adds a scaling transformation to the canvas units by x horizontally and by
+y vertically.
+
+By default, one unit on the canvas is exactly one pixel. If we apply, for
+instance, a scaling factor of 0.5, the resulting unit would become 0.5 pixels
+and so shapes would be drawn at half size. In a similar way setting the scaling
+factor to 2.0 would increase the unit size and one unit now becomes two pixels.
+This results in shapes being drawn twice as large.
+
+    scale x y
+
+  - x
+      - Scaling factor in the horizontal direction.
+  - y
+      - Scaling factor in the vertical direction.
+
+Note: You can use `scale -1 1` to flip the context horizontally and `scale 1
+-1` to flip it vertically.
+
+[MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/scale)
+
+-}
 scale : Float -> Float -> Command
 scale x y =
     fn "scale" [ float x, float y ]
 
 
-{-| -}
+{-| Sets the line dash pattern used when stroking lines, using an array of
+values which specify alternating lengths of lines and gaps which describe the
+pattern.
+
+To return to using solid lines, set the line dash list to an empty array.
+
+    setLineDash segments
+
+  - `segments`
+      - An Array of numbers which specify distances to alternately draw a line
+        and a gap (in coordinate space units). If the number of elements in the array
+        is odd, the elements of the array get copied and concatenated. For example, `[5,
+        15, 25]` will become `[5, 15, 25, 5, 15, 25]`. If the array is empty, the line
+        dash list is cleared and line strokes return to being solid.
+
+[MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/setLineDash)
+
+-}
 setLineDash : List Float -> Command
 setLineDash segments =
     fn "setLineDash" [ Encode.list (List.map float segments) ]
 
 
-{-| -}
+{-| Resets (overrides) the current transformation to the identity matrix and
+then invokes a transformation described by the arguments of this method.
+
+See also the `transform` method, which does not override the current transform
+matrix and multiplies it with a given one.
+
+    setTransform a b c d e f
+
+  - `a` (m11)
+      - Horizontal scaling.
+  - `b` (m12)
+      - Horizontal skewing.
+  - `c` (m21)
+      - Vertical skewing.
+  - `d` (m22)
+      - Vertical scaling.
+  - `e` (dx)
+      - Horizontal moving.
+  - `f` (dy)
+      - Vertical moving.
+
+[MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/setTransform)
+
+-}
 setTransform : Float -> Float -> Float -> Float -> Float -> Float -> Command
 setTransform a b c d e f =
     fn "setTransform" [ float a, float b, float c, float d, float e, float f ]
 
 
-{-| -}
+{-| Strokes the current or given path with the current stroke style using the
+non-zero winding rule. [MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/stroke)
+-}
 stroke : Command
 stroke =
     fn "stroke" []
 
 
-{-| -}
+{-| Paints a rectangle which has a starting point at (`x`, `y`) and has a `width`
+and an `height` onto the canvas, using the current stroke style. [MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/strokeRect)
+
+    strokeRect x y width height
+
+-}
 strokeRect : Float -> Float -> Float -> Float -> Command
 strokeRect x y w h =
     fn "strokeRect" [ float x, float y, float w, float h ]
 
 
-{-| -}
+{-| Strokes — that is, draws the outlines of — the characters of a specified
+text string at the given (`x`, `y`) position. If the optional fourth parameter
+for a maximum width is provided, the text is scaled to fit that width.
+
+See the `fillText` method to draw the text with the characters filled with
+color rather than having just their outlines drawn.
+
+    strokeText text x y maxWidth
+
+[MDNn docs](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/strokeText)
+
+    strokeText "Hello world" 40 50 (Just 100)
+
+-}
 strokeText : String -> Float -> Float -> Maybe Float -> Command
 strokeText text x y maxWidth =
     case maxWidth of
@@ -902,13 +1162,42 @@ strokeText text x y maxWidth =
             fn "strokeText" [ string text, float x, float y, float maxWidth ]
 
 
-{-| -}
+{-| Multiplies the current transformation with the matrix described by the
+arguments of this method. You are able to scale, rotate, move and skew the
+context.
+
+See also the `setTransform` method which resets the current transform to the
+identity matrix and then invokes `transform`.
+
+    transform a b c d e f
+
+  - `a` (m11)
+      - Horizontal scaling.
+  - `b` (m12)
+      - Horizontal skewing.
+  - `c` (m21)
+      - Vertical skewing.
+  - `d` (m22)
+      - Vertical scaling.
+  - `e` (dx)
+      - Horizontal moving.
+  - `f` (dy)
+      - Vertical moving.
+
+[MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/transform)
+
+-}
 transform : Float -> Float -> Float -> Float -> Float -> Float -> Command
 transform a b c d e f =
     fn "transform" [ float a, float b, float c, float d, float e, float f ]
 
 
-{-| -}
+{-| Adds a translation transformation by moving the canvas and its origin `x`
+horizontally and `y` vertically on the grid. [MDN docs](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/translate)
+
+    translate x y
+
+-}
 translate : Float -> Float -> Command
 translate x y =
     fn "translate" [ float x, float y ]
